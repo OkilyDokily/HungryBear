fae6e2cea287537e61a4889d820a321efe17ea1c
me <me@me.me.com>
2020-07-02 14:56:53 -0700

    implement tests for medium

--- __test__/hungrybear.test.js
+++ __test__/hungrybear.test.js
@@ -39,3 +39,44 @@ describe('Fuzzy', () => {
     expect(fuzzy.foodLevel).toEqual(10);
   });
 });
+
+
+describe('Fuzzy, easy', () => {
+  jest.useFakeTimers();
+  let fuzzy;
+
+  beforeEach(function() {
+    fuzzy = new HungryBear("Fuzzy", "easy");
+    fuzzy.setHunger();
+  });
+
+  afterEach(function() {
+    jest.clearAllTimers();
+  });
+
+  test('should have a name and a food level of 10 when it is created', () => {
+    expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  test('should have a food level of 7 after 4501 milliseconds', () => {
+    jest.advanceTimersByTime(4501);
+    expect(fuzzy.foodLevel).toEqual(7);
+  });
+
+  test('should get very hungry if the food level drops below zero', () => {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  test('should get very hungry if 15 seconds pass without feeding', () => {
+    jest.advanceTimersByTime(15001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  test('should have a food level of 15 if it is fed', () => {
+    jest.advanceTimersByTime(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).toEqual(15);
+  });
+});
